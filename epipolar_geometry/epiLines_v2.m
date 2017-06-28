a2aff11540db3ba5c14d325d14ee9fd8fb72f58a
HakonHystad <hakonhystad@gmail.com>
2017-06-28 11:12:14 +0200

    added epipole plot

--- epipolar_geometry/epiLines_v2.m
+++ epipolar_geometry/epiLines_v2.m
@@ -5,8 +5,11 @@
 
 %% load images
 % add two overlapping images here to see the effect of camera movement on epipolar lines.
-im1 = imread('images/building.jpg');
-im2 = imread('images/building2.jpg');
+% im1 = imread('images/building.jpg');
+% im2 = imread('images/building2.jpg');
+im1 = imread('images/book1a.jpg');
+im2 = imread('images/book2a.jpg');
+
 
 eGeometry = Epipolar( im1, im2 );
 
@@ -24,6 +27,8 @@ hold on
 eGeometry.plotInlierFeatures(1,len);
 % plot epipolar lines of image 1 due to points in image 2
 eGeometry.plotEpiLine(1,len);
+% plot epipole of image 1 due to camera center of image 2
+eGeometry.plotEpiPole(1);
 hold off
 
 %% image 2
@@ -31,6 +36,8 @@ subplot(1,2,2),imshow( im2 );
 hold on
 % plot inlier points of image 2
 eGeometry.plotInlierFeatures(2,len);
-% plot epipolar lines of image 1 due to points in image 2
+% plot epipolar lines of image 2 due to points in image 1
 eGeometry.plotEpiLine(2,len);
+% plot epipole of image 2 due to camera center of image 1
+eGeometry.plotEpiPole(2);
 hold off
