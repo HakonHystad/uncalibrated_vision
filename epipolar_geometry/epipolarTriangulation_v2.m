e32d0cbfdc522858163466a021ffc0c473d2bb4f
HakonHystad <hakonhystad@gmail.com>
2017-06-30 13:07:48 +0200

    updated to use baseclass

--- epipolar_geometry/epipolarTriangulation_v2.m
+++ epipolar_geometry/epipolarTriangulation_v2.m
@@ -3,19 +3,15 @@ imx = imread('images/shelf1.jpg');
 imxp = imread('images/shelf2.jpg');
 
 %% estimate the fundamental matrix
-[F,inliersX, inliersXP] = extractF(imx,imxp);
-
+epi = Epipolar(imx,imxp);
 %% get camera matrices
-% epipole in image 2.
-epipoleXP = null(F');
-epipoleXP = epipoleXP./norm(epipoleXP);
 % Let camera 1 be origio and camera 2 be a Canonical
 % decomposition, Hartley/Zisserman p.256
 PX = [eye(3),[  0   0   1   ]'];
-PXP = [ Skew(epipoleXP)*F, epipoleXP];
+PXP = [ Skew(epi.eP2)*epi.F, epi.eP2];
 
 %% triangulate inliers
-worldPts = triangulate2d(inliersX(:,1:2), inliersXP(:,1:2),PX,PXP); 
+worldPts = triangulate2d(epi.in1(:,1:2), epi.in2(:,1:2),PX,PXP); 
 
 %% show results
 imshow( imx );
@@ -26,19 +22,20 @@ hold on
 color = worldPts(:,3);
 [maxDepth,maxIdx] = max( color );
 [minDepth, minIdx] = min( color );
+fprintf('Depth span: %.2f\n', maxDepth-minDepth );
 color = color - minDepth;
 color = color/maxDepth;
 
 % mark extremes in depths
 distTxt = sprintf('(%.2f,%.2f,%.2f)', worldPts(minIdx,1),worldPts(minIdx,2),worldPts(minIdx,3));
-text( double( inliersX(minIdx,1) ), double( inliersX(minIdx,2) ), distTxt, 'Color', 'red','BackgroundColor','white' );
+text( double( epi.in1(minIdx,1) ), double( epi.in1(minIdx,2) ), distTxt, 'Color', 'red','BackgroundColor','white' );
 distTxt = sprintf('(%.2f,%.2f,%.2f)', worldPts(maxIdx,1),worldPts(maxIdx,2),worldPts(maxIdx,3));
-text( double( inliersX(maxIdx,1) ), double( inliersX(maxIdx,2) ), distTxt, 'Color', 'yellow','BackgroundColor','white' );
-plot( inliersX(minIdx,1), inliersX(minIdx,2), 'rs','MarkerSize',10 );
-plot( inliersX(maxIdx,1), inliersX(maxIdx,2), 'ys','MarkerSize',10 );
+text( double( epi.in1(maxIdx,1) ), double( epi.in1(maxIdx,2) ), distTxt, 'Color', 'yellow','BackgroundColor','white' );
+plot( epi.in1(minIdx,1), epi.in1(minIdx,2), 'rs','MarkerSize',10 );
+plot( epi.in1(maxIdx,1), epi.in1(maxIdx,2), 'ys','MarkerSize',10 );
 
 % plot inliers with depth gradient on top of image
-scatter( inliersX(:,1), inliersX(:,2), [], worldPts(:,3),'filled' ); 
+scatter( epi.in1(:,1), epi.in1(:,2), [], worldPts(:,3),'filled' ); 
 hold off
 
 % make a 3d plot of triangulated positions
