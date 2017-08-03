e552ad41272d4f257472a9a975d333c8ba6cf50e
HakonHystad <hakonhystad@gmail.com>
2017-08-03 14:22:06 +0200

    restructured

---
+++ main.m
@@ -0,0 +1,32 @@
+
+addpath( genpath('./') );
+
+app = { 'Single view recovery',...
+        'Stitching',...
+        '3D recovery',...
+        'Rectification',...
+        'Projective depth'};
+        
+
+choice = '';
+while true
+    choice = menu( 'Choose application',app{:}, 'Close');
+     switch choice
+         case 1
+             recoveryGUI
+         case 2
+             stitching_gui
+         case 3
+             affine3Dreconstruction_v2
+         case 4
+             rectificationGUI
+         case 5
+             projectiveDepthGUI
+         otherwise
+             break;
+     end% switch
+     
+     
+     
+ end% while 1
+     
